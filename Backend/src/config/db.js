28628ad8f0fc06ebbf1d4812ee590f55ed519d59
Jorgecorrea-uao <jorge.correa@uao.edu.co>
2026-05-02 19:17:31 -0500

    fix(config): actualizar configuracion de conexion a base de datos

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const { Sequelize } = require('sequelize')
-require('dotenv').config()
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 5432,
-        dialect: 'postgres',
-        logging: false
-    }
-)
-
-module.exports = sequelize
-=======
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -31,4 +13,3 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
->>>>>>> origin/main
